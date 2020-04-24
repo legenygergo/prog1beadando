5d91237a1c74137c8dfa89e64a50bc83101df9ee
legenygergo <50411029+legenygergo@users.noreply.github.com>
2020-04-24 12:55:49 +0200

    Add files via upload

--- 31es.py
+++ 31es.py
@@ -1,13 +1,38 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-m=[[1,1,1,1],
-   [1,1,1,1],
-   [1,1,1,1]]
-plt.matshow(m)
-plt.show(m)
-# bemenet = input("Adja meg a mátrix méreteit és szinét vesszővel elválasztva: ")
-# elso, masodik, szin=bemenet.split(",")
-# print(elso)
-# print(masodik)
-# print(szin)
+matrixmeret=input("Adja meg a mátrix méreteit, illetve a választott szin nevét space-ekkel elválasztva: ")
+n, m, szin = matrixmeret.split()
+
+piros = np.zeros([int(n), int(m), 3], dtype=np.uint8)
+piros[:,:] = [255, 0, 0]
+kek = np.zeros([int(n), int(m), 3], dtype=np.uint8)
+kek[:,:] = [0, 0, 255]
+sarga= np.zeros([int(n), int(m), 3], dtype=np.uint8)
+sarga[:,:] = [255, 255, 0]
+
+dict={"matrix":np.zeros([int(n), int(m), 3], dtype=np.uint8), "piros":piros[:,:], "kek": kek[:,:], "sarga":sarga[:,]}
+
+if szin=="lila":
+    lila=dict.get("matrix")
+    lila[:,:]=dict.get("piros")+dict.get("kek")
+    plt.imshow(lila)
+    plt.show()
+
+elif szin=="fehér":
+    feher=dict.get("matrix")
+    feher[:,:]=dict.get("sarga")+dict.get("kek")
+    plt.imshow(feher)
+    plt.show()
+
+elif szin=="zöld":
+    zold = dict.get("matrix")
+    zold[:, :] = dict.get("sarga") - dict.get("piros")
+    plt.imshow(zold)
+    plt.show()
+else:
+    s=dict.get("matrix")
+    s[:,:]=dict.get("sarga")
+    plt.imshow(s)
+    plt.show()
--- 9es.py
+++ 9es.py
@@ -1,3 +1,27 @@
+# T=int(input("Adja meg a tesztesetek számát (1-től 5000-ig): "))
+# szam=0
+# mehet=True
+# if T == szam or -1>=T or T>5000:
+#     print("T nem esik bele a megadott tartományba.")
+#     mehet=False
+#
+# while szam != T and mehet:
+#     szm=input("Adja meg az N sorok és M oszlopok számát elválasztva (1-től 50-ig): ")
+#     N, M=szm.split(" ")
+#     if int(N)>50 or 1>int(N) or int(M)>50 or 1>int(M):
+#         print("N vagy M nem esik bele a megadott tartományba.")
+#         break
+#     if (int(N) ==1 or int(M) ==1) or int(N)*int(M) % 2 !=0:
+#         print("No")
+#     else:
+#         print("Yes")
+#     szam+=1
+
+# A feladatból nem volt egyértelmű, hogy lehet-e cserélni az elemeket, úgy vettem hogy nem. Igy akkor tér vissza
+# Yes-sel, ha megfelel azoknak a kritériumoknak, hogy az M és N szorzata páros, illetve egyik sem lehet egyenlő
+# 1-el. Ha mégis lehetne cserélni, a különbség annyi lenne, hogy egy egysoros vagy egy oszlopos mátrixnál is
+# visszatérhetne Yes-el, amennyiben az egyik paraméter páros. Akkor a következőképpen néz ki a megoldás:
+
 
 T=int(input("Adja meg a tesztesetek számát (1-től 5000-ig): "))
 szam=0
@@ -12,13 +36,8 @@ while szam != T and mehet:
     if int(N)>50 or 1>int(N) or int(M)>50 or 1>int(M):
         print("N vagy M nem esik bele a megadott tartományba.")
         break
-    if (int(N) ==1 or int(M) ==1) or int(N)*int(M) % 2 !=0:
+    if int(N)*int(M) % 2 !=0:
         print("No")
     else:
         print("Yes")
     szam+=1
-
-# A feladatból nem volt egyértelmű, hogy lehet-e cserélni az elemeket, úgy vettem hogy nem. Igy akkor tér vissza
-# Yes-sel, ha megfelel azoknak a kritériumoknak, hogy az M és N szorzata páros, illetve egyik sem lehet egyenlő
-# 1-el. Ha mégis lehetne cserélni, a különbség annyi lenne, hogy egy egysoros vagy egy oszlopos mátrixnál is
-# visszatérhetne Yes-el, amennyiben az egyik paraméter páros.
